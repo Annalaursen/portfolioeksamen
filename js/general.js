4554a087a9705d6d328d2ef0f21b6452e89b1315
Anna Laursen <anla1007@stud.ek.dk>
2026-06-07 14:11:50 +0200

    opsætning projektside + js

--- js/general.js
+++ js/general.js
@@ -1,1 +1,17 @@
-/* Burger menu */
+// projektsiden
+
+function visTema(temaId, knap) {
+  const alleTemaer = document.querySelectorAll(".tema");
+  const alleKnapper = document.querySelectorAll(".tema-btn");
+
+  alleTemaer.forEach((tema) => {
+    tema.classList.remove("active-tema");
+  });
+
+  alleKnapper.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  document.getElementById(temaId).classList.add("active-tema");
+  knap.classList.add("active");
+}
